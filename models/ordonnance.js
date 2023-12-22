3d85636fc70242dfe99de755a8ad57aac8b21c2a
Ismael <ismael.ait@protonmail.com>
2023-12-22 12:03:11 +0100

    Design rework medecins, pharmaciens, patients

--- models/ordonnance.js
+++ models/ordonnance.js
@@ -3,17 +3,17 @@ const db = require('../db/db');
 const Ordonnance = {
   getAllOrdonnances: (callback) => {
     const query = `
-      SELECT Ordonnance_Id, Medecin_Nom AS MedecinNom, Patient_Nom AS PatientNom, 
-             Maladie_Nom AS MaladieNom, DATE_FORMAT(Ordonnance_Date, '%d-%m-%Y') AS FormattedDate
+      SELECT Ordonnance_Id, Medecin.Medecin_Nom AS MedecinNom, Patient.Patient_Nom AS PatientNom, 
+             Maladie.Maladie_Nom AS MaladieNom, DATE_FORMAT(Ordonnance.Ordonnance_Date, '%d-%m-%Y') AS FormattedDate
       FROM Ordonnance
       INNER JOIN Medecin ON Ordonnance.Ordonnance_IdMedecin = Medecin.Medecin_Id
       INNER JOIN Patient ON Ordonnance.Ordonnance_IdPatient = Patient.Patient_Id
       INNER JOIN Maladie ON Ordonnance.Ordonnance_IdMaladie = Maladie.Maladie_Id;
     `;
     db.query(query, callback);
-  }
+  },
 
-  /*addOrdonnance: (newOrdonnance, callback) => {
+   addOrdonnance: (newOrdonnance, callback) => {
     db.query('INSERT INTO Ordonnance SET ?', newOrdonnance, callback);
   },
 
@@ -34,7 +34,6 @@ const Ordonnance = {
   deleteOrdonnance: (ordonnanceId, callback) => {
     db.query('DELETE FROM Ordonnance WHERE Ordonnance_Id = ?', [ordonnanceId], callback);
   }
-}*/
-}; 
+};
 
 module.exports = Ordonnance;
